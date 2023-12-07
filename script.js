835d9cf52c45183d77022ffa80b75239d8b738fd
vpm0001 <144981186+vpm0001@users.noreply.github.com>
2023-12-07 12:33:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const cursor = document.querySelector('.cursor');
+document.addEventListener('mousemove',function(e){
+    let x = e.clientX;
+    let y = e.clientY;
+
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+
+})
